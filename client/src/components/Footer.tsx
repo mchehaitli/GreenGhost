fb454860f2d6b4845f2050d29063380d4893f036
mchehaitli1 <39180726-mchehaitli1@users.noreply.replit.com>
2025-02-26 03:59:04 +0000

    Adjust Footer social media icon positioning
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/c891fb7f-7d80-4d21-877a-a109234f95d5/d85c57c8-bfb6-4e61-9b20-546bacedf5bb.jpg

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -28,8 +28,8 @@ const Footer = () => {
                 </div>
               </Link>
 
-              {/* Social Media Icons */}
-              <div className="flex space-x-4 mt-4">
+              {/* Social Media Icons - Moved to be more centered and slightly to the right */}
+              <div className="flex justify-center md:justify-start space-x-5 mt-5 pl-4">
                 <a href="#" className="text-muted-foreground hover:text-primary transition-colors duration-300" aria-label="Instagram">
                   <Instagram size={20} />
                 </a>
